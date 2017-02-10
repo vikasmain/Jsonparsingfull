3d1cb97cafd412f992223c0810246a5ab5ce0be3
vikasBajpayee <9716588439vik@gmail.com>
2017-02-10 06:58:27 +0530

    viewemployeesactivities

---
+++ app/src/main/java/com/example/dell/wasa/ViewAllEmployee.java
@@ -0,0 +1,104 @@
+package com.example.vikas.wasa;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ViewAllEmployee extends AppCompatActivity implements ListView.OnItemClickListener {
+
+    private ListView listView;
+
+    private String JSON_STRING;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_all_employee);
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setOnItemClickListener(this);
+        getJSON();
+    }
+
+
+    private void showEmployee(){
+        JSONObject jsonObject = null;
+        ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String, String>>();
+        try {
+            jsonObject = new JSONObject(JSON_STRING);
+            JSONArray result = jsonObject.getJSONArray(Config.TAG_JSON_ARRAY);
+
+            for(int i = 0; i<result.length(); i++){
+                JSONObject jo = result.getJSONObject(i);
+                String id = jo.getString(Config.TAG_ID);
+                String name = jo.getString(Config.TAG_NAME);
+
+                HashMap<String,String> employees = new HashMap<>();
+                employees.put(Config.TAG_ID,id);
+                employees.put(Config.TAG_NAME,name);
+                list.add(employees);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        ListAdapter adapter = new SimpleAdapter(
+                ViewAllEmployee.this, list, R.layout.list_item,
+                new String[]{Config.TAG_ID,Config.TAG_NAME},
+                new int[]{R.id.id, R.id.name});
+
+        listView.setAdapter(adapter);
+    }
+
+    private void getJSON(){
+        class GetJSON extends AsyncTask<Void,Void,String> {
+
+            ProgressDialog loading;
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(ViewAllEmployee.this,"Fetching Data","Wait...",false,false);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                JSON_STRING = s;
+                showEmployee();
+            }
+
+            @Override
+            protected String doInBackground(Void... params) {
+                RequestHandler rh = new RequestHandler();
+                String s = rh.sendGetRequest(Config.URL_GET_ALL);
+                return s;
+            }
+        }
+        GetJSON gj = new GetJSON();
+        gj.execute();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent = new Intent(this, ViewEmployee.class);
+        HashMap<String,String> map =(HashMap)parent.getItemAtPosition(position);
+        String empId = map.get(Config.TAG_ID).toString();
+        intent.putExtra(Config.EMP_ID,empId);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/dell/wasa/ViewEmployee.java
@@ -0,0 +1,209 @@
+package com.example.vikas.wasa;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class ViewEmployee extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editTextId;
+    private EditText editTextName;
+    private EditText editTextDesg;
+    private EditText editTextSalary;
+
+    private Button buttonUpdate;
+    private Button buttonDelete;
+
+    private String id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_employee);
+
+        Intent intent = getIntent();
+
+        id = intent.getStringExtra(Config.EMP_ID);
+
+        editTextId = (EditText) findViewById(R.id.editTextid);
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextDesg = (EditText) findViewById(R.id.editTextDesg);
+        editTextSalary = (EditText) findViewById(R.id.editTextSalary);
+
+        buttonUpdate = (Button) findViewById(R.id.buttonupd);
+        buttonDelete = (Button) findViewById(R.id.buttondel);
+
+        buttonUpdate.setOnClickListener(this);
+        buttonDelete.setOnClickListener(this);
+
+        editTextId.setText(id);
+
+        getEmployee();
+    }
+
+    private void getEmployee(){
+        class GetEmployee extends AsyncTask<Void,Void,String> {
+            ProgressDialog loading;
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(ViewEmployee.this,"Fetching...","Wait...",false,false);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                showEmployee(s);
+            }
+
+            @Override
+            protected String doInBackground(Void... params) {
+                RequestHandler rh = new RequestHandler();
+                String s = rh.sendGetRequestParam(Config.URL_GET_EMP,id);
+                return s;
+            }
+        }
+        GetEmployee ge = new GetEmployee();
+        ge.execute();
+    }
+
+    private void showEmployee(String json){
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            JSONArray result = jsonObject.getJSONArray(Config.TAG_JSON_ARRAY);
+            JSONObject c = result.getJSONObject(0);
+            String name = c.getString(Config.TAG_NAME);
+            String desg = c.getString(Config.TAG_DESG);
+            String sal = c.getString(Config.TAG_SAL);
+
+            editTextName.setText(name);
+            editTextDesg.setText(desg);
+            editTextSalary.setText(sal);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void updateEmployee(){
+        final String name = editTextName.getText().toString().trim();
+        final String desg = editTextDesg.getText().toString().trim();
+        final String salary = editTextSalary.getText().toString().trim();
+
+        class UpdateEmployee extends AsyncTask<Void,Void,String> {
+            ProgressDialog loading;
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(ViewEmployee.this,"Updating...","Wait...",false,false);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(ViewEmployee.this,s, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(Void... params) {
+                HashMap<String,String> hashMap = new HashMap<>();
+                hashMap.put(Config.KEY_EMP_ID,id);
+                hashMap.put(Config.KEY_EMP_NAME,name);
+                hashMap.put(Config.KEY_EMP_DESG,desg);
+                hashMap.put(Config.KEY_EMP_SAL,salary);
+
+                RequestHandler rh = new RequestHandler();
+
+                String s = rh.sendPostRequest(Config.URL_UPDATE_EMP,hashMap);
+
+                return s;
+            }
+        }
+
+        UpdateEmployee ue = new UpdateEmployee();
+        ue.execute();
+    }
+
+    private void deleteEmployee(){
+        class DeleteEmployee extends AsyncTask<Void,Void,String> {
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(ViewEmployee.this, "Updating...", "Wait...", false, false);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(ViewEmployee.this, s, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(Void... params) {
+                RequestHandler rh = new RequestHandler();
+                String s = rh.sendGetRequestParam(Config.URL_DELETE_EMP, id);
+                return s;
+            }
+        }
+
+        DeleteEmployee de = new DeleteEmployee();
+        de.execute();
+    }
+
+    private void confirmDeleteEmployee(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("Are you sure you want to delete this employee?");
+
+        alertDialogBuilder.setPositiveButton("Yes",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        deleteEmployee();
+                        startActivity(new Intent(ViewEmployee.this,ViewAllEmployee.class));
+                    }
+                });
+
+        alertDialogBuilder.setNegativeButton("No",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+
+                    }
+                });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == buttonUpdate){
+
+            updateEmployee();
+        }
+
+        if(v == buttonDelete){
+            confirmDeleteEmployee();
+        }
+    }
+}
