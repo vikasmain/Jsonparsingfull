6a22b962017b6c54e699c3bf3dd981e70eaf6b43
vikasBajpayee <9716588439vik@gmail.com>
2017-02-10 06:55:58 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/dell/wasa/myswl.java
@@ -0,0 +1,95 @@
+package com.example.vikas.wasa;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
+public class myswl extends AppCompatActivity implements View.OnClickListener{
+
+    //Defining views
+    private EditText editTextName;
+    private EditText editTextDesg;
+    private EditText editTextSal;
+
+    private Button buttonAdd;
+    private Button buttonView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swl);
+
+        //Initializing views
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextDesg = (EditText) findViewById(R.id.editTextDesg);
+        editTextSal = (EditText) findViewById(R.id.editTextSalary);
+
+        buttonAdd = (Button) findViewById(R.id.buttonAdd);
+        buttonView = (Button) findViewById(R.id.buttonView);
+
+        //Setting listeners to button
+        buttonAdd.setOnClickListener(this);
+        buttonView.setOnClickListener(this);
+    }
+
+
+    //Adding an employee
+    private void addEmployee(){
+
+        final String name = editTextName.getText().toString().trim();
+        final String desg = editTextDesg.getText().toString().trim();
+        final String sal = editTextSal.getText().toString().trim();
+
+        class AddEmployee extends AsyncTask<Void,Void,String>{
+
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(myswl.this,"Adding...","Wait...",false,false);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(myswl.this,s,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(Void... v) {
+                HashMap<String,String> params = new HashMap<>();
+                params.put(Config.KEY_EMP_NAME,name);
+                params.put(Config.KEY_EMP_DESG,desg);
+                params.put(Config.KEY_EMP_SAL,sal);
+
+                RequestHandler rh = new RequestHandler();
+                String res = rh.sendPostRequest(Config.URL_ADD, params);
+                return res;
+            }
+        }
+
+        AddEmployee ae = new AddEmployee();
+        ae.execute();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == buttonAdd){
+            addEmployee();
+        }
+
+        if(v == buttonView){
+            startActivity(new Intent(this,ViewAllEmployee.class));
+        }
+    }
+}
