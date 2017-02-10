65b4b36f589d1ff9a0b397c94ab988acb7d5f496
vikasBajpayee <9716588439vik@gmail.com>
2017-02-10 06:57:03 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/dell/wasa/Player.java
@@ -0,0 +1,27 @@
+package com.example.vikas.wasa;
+/**
+ * Created by Hp on 3/17/2016.
+ */
+public class Player {
+    private String name;
+    private String pos;
+    private int img;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPos() {
+        return pos;
+    }
+    public void setPos(String pos) {
+        this.pos = pos;
+    }
+    public int getImg() {
+        return img;
+    }
+    public void setImg(int img) {
+        this.img = img;
+    }
+}
